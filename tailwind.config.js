bbe343d9e89702a10c716f3d0e407eb438b868a0
Charles-W <chwc17@gmail.com>
2025-04-17 19:07:05 -0400

    fic tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,7 @@
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
-  safelist: [
-    'bg-red-500',
-    'bg-blue-500',
-    'bg-green-500',
-    'text-white',
-    'text-xl',
-    'p-4',
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        base: ['Bellefair', 'serif'],
-        title: ['Perandory', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
